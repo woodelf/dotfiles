15ea4ade09a8e7c541288d4fe48efa8d5ce40e3c
woodelf <wood_elf@126.com>
2018-03-06 22:21:30 +0800

    dotfiles update

--- obmenu-generator/schema.pl
+++ obmenu-generator/schema.pl
@@ -8,9 +8,10 @@
     cat:       add a category inside the menu             {cat => ["name", "label", "icon"]},
     sep:       horizontal line separator                  {sep => undef}, {sep => "label"},
     pipe:      a pipe menu entry                         {pipe => ["command", "label", "icon"]},
-    raw:       any valid Openbox XML string               {raw => q(xml string)},
-    begin_cat: begin of a category                  {begin_cat => ["name", "icon"]},
-    end_cat:   end of a category                      {end_cat => undef},
+    file:      include the content of an XML file        {file => "/path/to/file.xml"},
+    raw:       any XML data supported by Openbox          {raw => q(...)},
+    beg:       begin of a category                        {beg => ["name", "icon"]},
+    end:       end of a category                          {end => undef},
     obgenmenu: generic menu settings                {obgenmenu => ["label", "icon"]},
     exit:      default "Exit" action                     {exit => ["label", "icon"]},
 
@@ -27,90 +28,82 @@ require "$ENV{HOME}/.config/obmenu-generator/config.pl";
 my $editor = $CONFIG->{editor};
 
 our $SCHEMA = [
-    #          COMMAND                 LABEL                ICON
-    {item => ['xdg-open .',        '文件管理器',        'file-manager']},
-    {item => ['sakura',            '终端',              'terminal']},
-    {item => ['xdg-open http://',  '网络浏览器',        'web-browser']},
-    {item => ['gmrun',             '运行命令',          'system-run']},
-    {item => ['pidgin',            '即时通信',          'system-users']},
 
-    {sep => '应用程序'},
+    #          COMMAND                 LABEL              ICON
+    {item => ['xdg-open .',       '文件管理器', 'system-file-manager']},
+    {item => ['sakura',           '终端',     'utilities-terminal']},
+    {item => ['xdg-open http://', '网络浏览器',  'web-browser']},
+    {item => ['gmrun',            '运行命令',  'system-run']},
+
+    {sep => '分类'},
 
     #          NAME            LABEL                ICON
-    {cat => ['utility',     '附件',        'applications-utilities']},
-    {cat => ['development', '开发',        'applications-development']},
-    {cat => ['education',   '教育',        'applications-science']},
-    {cat => ['game',        '游戏',        'applications-games']},
-    {cat => ['graphics',    '图形',        'applications-graphics']},
-    {cat => ['audiovideo',  '多媒体',      'applications-multimedia']},
-    {cat => ['network',     '网络',        'applications-internet']},
-    {cat => ['office',      '办公',        'applications-office']},
-    {cat => ['other',       '其他',        'applications-other']},
-    {cat => ['settings',    '设置',        'applications-accessories']},
-    {cat => ['system',      '系统',        'applications-system']},
-
-    #{cat => ['qt',          'QT Applications',    'qt4logo']},
-    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
-    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
-    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
-    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
+    {cat => ['utility',     '附件', 'applications-utilities']},
+    {cat => ['development', '开发', 'applications-development']},
+    {cat => ['education',   '教育', 'applications-science']},
+    {cat => ['game',        '游戏', 'applications-games']},
+    {cat => ['graphics',    '图形', 'applications-graphics']},
+    {cat => ['audiovideo',  '多媒体',  'applications-multimedia']},
+    {cat => ['network',     '网络', 'applications-internet']},
+    {cat => ['office',      '办公', 'applications-office']},
+    {cat => ['other',       '其他', 'applications-other']},
+    {cat => ['settings',    '设置', 'applications-accessories']},
+    {cat => ['system',      '系统', 'applications-system']},
 
     #                  LABEL          ICON
-    #{begin_cat => ['My category',  'cat-icon']},
-    #             ... some items ...
-    #{end_cat   => undef},
+    #{beg => ['My category',  'cat-icon']},
+    #          ... some items ...
+    #{end => undef},
 
     #            COMMAND     LABEL        ICON
     #{pipe => ['obbrowser', 'Disk', 'drive-harddisk']},
 
     ## Generic advanced settings
     #{sep       => undef},
-    #{obgenmenu => ['Openbox Settings', 'openbox']},
+    #{obgenmenu => ['Openbox Settings', 'applications-engineering']},
     #{sep       => undef},
 
     ## Custom advanced settings
     {sep => undef},
-    {begin_cat => ['高级设置', 'gnome-settings']},
+    {beg => ['高级设置', 'applications-engineering']},
 
-        # Configuration files
-        #{item      => ["$editor ~/.conkyrc",              'Conky RC',    $editor]},
-        {item      => ["$editor ~/.config/tint2/tint2rc", 'Tint2 面板', $editor]},
+      # Configuration files
+      {item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-generic']},
+      {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 面板', 'text-x-generic']},
 
-        # obmenu-generator category
-        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
-            {item      => ["$editor ~/.config/obmenu-generator/schema.pl", '菜单结构', $editor]},
-            {item      => ["$editor ~/.config/obmenu-generator/config.pl", '菜单配置', $editor]},
+      # obmenu-generator category
+      {beg => ['Obmenu-Generator', 'accessories-text-editor']},
+        {item => ["$editor ~/.config/obmenu-generator/schema.pl", '菜单结构', 'text-x-generic']},
+        {item => ["$editor ~/.config/obmenu-generator/config.pl", '菜单配置', 'text-x-generic']},
 
-            {sep  => undef},
-            {item => ['obmenu-generator -p',    '生成管道菜单',                      'menu-editor']},
-            {item => ['obmenu-generator -s',    '生成静态菜单',                      'menu-editor']},
-            {item => ['obmenu-generator -p -i', '生成带图标的管道菜单',              'menu-editor']},
-            {item => ['obmenu-generator -s -i', '生成带图标的静态菜单',              'menu-editor']},
-            {sep  => undef},
+        {sep  => undef},
+        {item => ['obmenu-generator -s -c',    '生成静态菜单',             'accessories-text-editor']},
+        {item => ['obmenu-generator -s -i -c', '生成带图标的静态菜单',  'accessories-text-editor']},
+        {sep  => undef},
+        {item => ['obmenu-generator -p',       '生成动态菜单',            'accessories-text-editor']},
+        {item => ['obmenu-generator -p -i',    '生成带图标的动态菜单', 'accessories-text-editor']},
+        {sep  => undef},
 
-            {item    => ['obmenu-generator -d', '刷新图标集', 'gtk-refresh']},
-        {end_cat => undef},
+        {item => ['obmenu-generator -d', '刷新缓存', 'view-refresh']},
+      {end => undef},
 
-        # Openbox category
-        {begin_cat => ['Openbox', 'openbox']},
-            {item      => ['openbox --reconfigure',               '重新配置 Openbox',   'openbox']},
-            {item      => ["$editor ~/.config/openbox/autostart", 'Openbox 自启动程序',   $editor]},
-            {item      => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',           $editor]},
-            {item      => ["$editor ~/.config/openbox/menu.xml",  'Openbox 菜单',         $editor]},
-        {end_cat => undef},
+      # Openbox category
+      {beg => ['Openbox', 'openbox']},
+        {item => ["$editor ~/.config/openbox/autostart", 'Openbox 自启动',   'text-x-generic']},
+        {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-x-generic']},
+        {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox 菜单',        'text-x-generic']},
+        {item => ['openbox --reconfigure',               '重新配置 Openbox', 'openbox']},
+      {end => undef},
+    {end => undef},
 
-        # Pacman category
-        #{begin_cat => ['Pacman / Servers', 'package-manager-icon']},
-        #    {item      => ["gksu $editor /etc/makepkg.conf",        'Makepkg Config',    'start-here']},
-        #    {item      => ["gksu $editor /etc/pacman.conf",         'Pacman Config',     'start-here']},
-        #    {item      => ["gksu $editor /etc/pacman.d/mirrorlist", 'Pacman Mirrorlist', 'start-here']},
-        #{end_cat => undef},
-    {end_cat => undef},
     {sep => undef},
 
     ## The xscreensaver lock command
-    {item => ['xscreensaver-command -lock', '锁定', 'lock']},
+    {item => ['xscreensaver-command -lock', '锁定', 'system-lock-screen']},
 
-    # This option uses the default Openbox's action "Exit"
-    {exit => ['退出', 'exit']},
+    ## This option uses the default Openbox's "Exit" action
+    {exit => ['退出', 'application-exit']},
+
+    ## This uses the 'oblogout' menu
+    # {item => ['oblogout', 'Exit', 'application-exit']},
 ]
